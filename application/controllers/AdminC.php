d5b4ed59c77574452b60ff91c071ea46d69979e9
ferryf17 <ffpamanah@gmail.com>
2018-12-13 10:03:23 +0700

    admin

--- application/controllers/AdminC.php
+++ application/controllers/AdminC.php
@@ -1,16 +1,50 @@
 <?php
+
 defined('BASEPATH') or exit('No direct script access allowed');
-class AdminC extends CI_Controller{
-	var $data = array();
-	public function __construct(){
-		parent::__construct();  
-		in_access();
-		admin_access();
-		$this->load->model('LoginM');
-	}
-	public function index(){
-		$data['title'] = "BTKP - Home";
-		$data['isi'] = $this->load->view('workshop/Home_v',$this->data, TRUE);
-		$this->load->view('workshop/Layout', $data);
-	}
+class AdminC extends CI_Controller
+{
+    public $data = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+        in_access();
+        admin_access();
+        $this->load->model('LoginM');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'BTKP - Home';
+        $data['isi'] = $this->load->view('admin/dashboard_v', $this->data, true);
+        $this->load->view('admin/Layout', $data);
+    }
+
+    public function masterdata()
+    {
+        $data['title'] = 'BTKP - Data User';
+        $data['isi'] = $this->load->view('admin/masterdata_v', $this->data, true);
+        $this->load->view('admin/Layout', $data);
+    }
+
+    public function datauser()
+    {
+        $data['title'] = 'BTKP - Data User';
+        $data['isi'] = $this->load->view('admin/datauser_v', $this->data, true);
+        $this->load->view('admin/Layout', $data);
+    }
+
+    public function datapegawai()
+    {
+        $data['title'] = 'BTKP - Data Pegawai';
+        $data['isi'] = $this->load->view('admin/datapegawai_v', $this->data, true);
+        $this->load->view('admin/Layout', $data);
+    }
+
+    public function dataalat_v()
+    {
+        $data['title'] = 'BTKP - Data Pegawai';
+        $data['isi'] = $this->load->view('admin/datapegawai_v', $this->data, true);
+        $this->load->view('admin/Layout', $data);
+    }
 }
