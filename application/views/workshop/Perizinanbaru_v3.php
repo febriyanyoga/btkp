503912d3d438fcef1d253b933bda98f20d15741c
ferryf17 <ffpamanah@gmail.com>
2019-01-08 09:51:45 +0700

    data

--- application/views/workshop/Perizinanbaru_v3.php
+++ application/views/workshop/Perizinanbaru_v3.php
@@ -40,62 +40,61 @@
 								<div class="">
 									<div class="" id="tab1" class="active show">
 										<?php
-										$data=$this->session->flashdata('sukses');
-										if($data!=""){ 
-											?>
+                                        $data = $this->session->flashdata('sukses');
+                                        if ($data != '') {
+                                            ?>
 											<div class="alert alert-success">
 												<button style="position: relative;" type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"></span></button>
 												<h3 style="color: white;"><i class="fa fa-check-circle"></i> Sukses!</h3>
-												<?=$data;?>
+												<?=$data; ?>
 											</div>
-											<?php 
-										} 
-										?>
-										<?php 
-										$data2=$this->session->flashdata('error');
-										if($data2!=""){ 
-											?>
+											<?php
+                                        }
+                                        ?>
+										<?php
+                                        $data2 = $this->session->flashdata('error');
+                                        if ($data2 != '') {
+                                            ?>
 											<div class="alert alert-danger">
 												<button style="position: relative;" type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true"></span></button>
 												<h3 style="color: white;"><i class="fa fa-check-circle"></i> Gagal!</h3>
-												<?=$data2;?>
+												<?=$data2; ?>
 											</div>
-											<?php 
-										} 
-										?>
+											<?php
+                                        }
+                                        ?>
 										<div class="section-title mt-5 mb-5">
 											<h4>Confirmation</h4>
 										</div>
 										<div id="accordion-icon-right" class="accordion">
 											<?php
-											foreach ($perizinan as $per) {
-												?>
+                                            foreach ($perizinan as $per) {
+                                                ?>
 												<div class="widget has-shadow">
 													<a class="card-header collapsed d-flex align-items-center" data-toggle="collapse" href="#IconRightCollapseOne" aria-expanded="true"><div class="card-title w-100">1. Identitas Service Station</div></a>
 													<div id="IconRightCollapseOne" class="card-body collapse show" data-parent="#accordion-icon-right">
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Nama Perusahaan</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_perusahaan?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_perusahaan; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<?php
-															$alamat_pt_detail = $this->WorkshopM->detail_alamat($per->id_kel_perusahaan)->row();
-															$alamat_ws_detail = $this->WorkshopM->detail_alamat($per->id_kel_workshop)->row();
-															?>
+                                                            $alamat_pt_detail = $this->WorkshopM->detail_alamat($per->id_kel_perusahaan)->row();
+                                                $alamat_ws_detail = $this->WorkshopM->detail_alamat($per->id_kel_workshop)->row(); ?>
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Alamat Kantor Perusahaan</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->alamat_perusahaan.'<br>'.$alamat_pt_detail->nama_kelurahan.', '.$alamat_pt_detail->nama_kecamatan.', '.$alamat_pt_detail->nama_kabupaten_kota.', '.$alamat_pt_detail->nama_propinsi;?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->alamat_perusahaan.'<br>'.$alamat_pt_detail->nama_kelurahan.', '.$alamat_pt_detail->nama_kecamatan.', '.$alamat_pt_detail->nama_kabupaten_kota.', '.$alamat_pt_detail->nama_propinsi; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Alamat Workshop/Service Station</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->alamat_workshop.'<br>'.$alamat_ws_detail->nama_kelurahan.', '.$alamat_ws_detail->nama_kecamatan.', '.$alamat_ws_detail->nama_kabupaten_kota.', '.$alamat_ws_detail->nama_propinsi;?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->alamat_workshop.'<br>'.$alamat_ws_detail->nama_kelurahan.', '.$alamat_ws_detail->nama_kecamatan.', '.$alamat_ws_detail->nama_kabupaten_kota.', '.$alamat_ws_detail->nama_propinsi; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Akta Pendirian Perusahaan</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->akta_perusahaan?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->akta_perusahaan; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Pemimpin/Penanggung Jawab</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_pimpinan?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_pimpinan; ?></div>
 														</div>
 													</div>
 													<a class="card-header collapsed d-flex align-items-center" data-toggle="collapse" href="#IconRightCollapseTwo">
@@ -104,15 +103,15 @@
 													<div id="IconRightCollapseTwo" class="card-body collapse" data-parent="#accordion-icon-right">
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Nomor Telepon Perusahaan</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->no_tlp?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->no_tlp; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Email Perusahaan</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->email_perusahaan?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->email_perusahaan; ?></div>
 														</div>
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">NPWP</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->npwp?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->npwp; ?></div>
 														</div>
 													</div>
 													<a class="card-header collapsed d-flex align-items-center" data-toggle="collapse" href="#IconRightCollapseThree">
@@ -121,7 +120,7 @@
 													<div id="IconRightCollapseThree" class="card-body collapse" data-parent="#accordion-icon-right">
 														<div class="form-group row mb-5">
 															<div class="col-sm-3 form-control-label d-flex align-items-center">Jenis SPK</div>
-															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_alat?></div>
+															<div class="col-sm-8 form-control-plaintext"><?php echo $per->nama_alat; ?></div>
 														</div>
 													</div>
 													<a class="card-header collapsed d-flex align-items-center" data-toggle="collapse" href="#IconRightCollapseFour">
@@ -129,38 +128,36 @@
 													</a>
 													<div id="IconRightCollapseFour" class="card-body collapse" data-parent="#accordion-icon-right">
 														<?php
-														$detail_berkas = $this->TatausahaM->get_berkas_by_id($per->id_perizinan)->result();
-														$i=11;
-														$j=0;
-														foreach ($detail_berkas as $ber){
-															$j++;
-															?>
+                                                        $detail_berkas = $this->TatausahaM->get_berkas_by_id($per->id_perizinan)->result();
+                                                $i = 11;
+                                                $j = 0;
+                                                foreach ($detail_berkas as $ber) {
+                                                    ++$j; ?>
 															<div class="form-group row mb-5">
 																<div class="col-xl-4">
 																	<div>
-																		<label for="<?php echo $i?>"><?php echo $j.'). &nbsp;'.$ber->nama_berkas?></label>
+																		<label for="<?php echo $i; ?>"><?php echo $j.'). &nbsp;'.$ber->nama_berkas; ?></label>
 																	</div>
 																</div>
 																<div class="col-xl-8">
-																	<a target="_blank" href="<?php echo base_url().'assets/upload/'.$ber->nama_file;?>" class="btn btn-primary btn-sm mr-1 mb-2"><i class="la la-eye"></i>Lihat Dokumen</i></a>
+																	<a target="_blank" href="<?php echo base_url().'assets/upload/'.$ber->nama_file; ?>" class="btn btn-primary btn-sm mr-1 mb-2"><i class="la la-eye"></i>Lihat Dokumen</i></a>
 																</div>
 															</div>
 															<?php
-															$i++;
-														}
-														?>
+                                                            ++$i;
+                                                } ?>
 													</div>
 												</div>
 												<?php
-											}
-											?>
+                                            }
+                                            ?>
 										</div>
 										<ul class="pager wizard text-right">
 											<!-- <li class="previous d-inline-block">
-												<a href="<?php echo site_url('izin_baru2/'.$id_perizinan)?>" class="btn btn-secondary ripple">Kembali</a>
+												<a href="<?php echo site_url('izin_baru2/'.$id_perizinan); ?>" class="btn btn-secondary ripple">Kembali</a>
 											</li> -->
 											<li class="next d-inline-block">
-												<a href="<?php echo site_url('selesai/'.$id_perizinan)?>" class="btn btn-gradient-01" onClick="return confirm('Anda yakin data yang anda isikan sudah benar?')">Selesai</a>
+												<a href="<?php echo site_url('selesai/'.$id_perizinan); ?>" class="btn btn-gradient-01" onClick="return confirm('Anda yakin data yang anda isikan sudah benar?')">Selesai</a>
 											</li>
 										</ul>
 									</div>
